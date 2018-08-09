e9e79f39568d9afa5deb9da8cb163e4a05d5e8a6
Guillaume Grossetie <ggrossetie@gmail.com>
2018-08-09 12:51:05 +0200

    Introduce a simple converter to HTML
    - New test cases with an input (AsciiDoc syntax) and an expected output (HTML)

--- lib/asciidoctor/inline_parser/node_extensions.rb
+++ lib/asciidoctor/inline_parser/node_extensions.rb
@@ -1,14 +1,34 @@
 module AsciidoctorGrammar
+  # Text
   class Text < ::Treetop::Runtime::SyntaxNode
+    def to_html
+      @comprehensive_elements.first.to_html
+    end
   end
 
   class Sentence < ::Treetop::Runtime::SyntaxNode
   end
 
   class Expression < ::Treetop::Runtime::SyntaxNode
+    def to_html
+      raw_text = text_value
+      if @comprehensive_elements.empty?
+        raw_text
+      else
+        @comprehensive_elements.reverse_each do |el|
+          unless el.instance_of? ::Treetop::Runtime::SyntaxNode
+            raw_text[el.interval] = el.to_html
+          end
+        end
+      end
+      raw_text
+    end
   end
 
   class QuotedContent < ::Treetop::Runtime::SyntaxNode
+    def to_html
+      @comprehensive_elements.first.to_html
+    end
   end
 
   # Quoted node
@@ -16,12 +36,22 @@ module AsciidoctorGrammar
     def content
       @elements.select { |el| el.instance_of? ::AsciidoctorGrammar::QuotedContent }.first.text_value
     end
+
+    def nested?
+      @elements.any? { |el| el.class.ancestors.include? ::AsciidoctorGrammar::QuotedNode }
+    end
   end
 
   class StrongQuoted < ::AsciidoctorGrammar::QuotedNode
+    def to_html
+      "<strong>#{@comprehensive_elements.first.to_html}</strong>"
+    end
   end
 
   class EmphasisQuoted < ::AsciidoctorGrammar::QuotedNode
+    def to_html
+      "<em>#{@comprehensive_elements.first.to_html}</em>"
+    end
   end
 
   class MonospacedQuoted < ::AsciidoctorGrammar::QuotedNode
---
+++ test/scenarios_test.rb
@@ -0,0 +1,23 @@
+require_relative 'test_helper'
+require 'asciidoctor'
+require 'asciidoctor/inline_parser/treetop_parser'
+
+describe 'scenario' do
+  let(:doc) { ::Asciidoctor::InlineParser.parse input }
+
+  Dir.chdir File.join __dir__, 'scenarios' do
+    (Dir.glob '**/*.adoc').each do |input_filename|
+      input_stem = input_filename.slice 0, input_filename.length - 5
+      scenario_name = input_stem.gsub '/', '::'
+      input_filename = File.absolute_path input_filename
+      output_filename = File.absolute_path %(#{input_stem}.html)
+      describe %(for #{scenario_name}) do
+        let(:input) { IO.read input_filename, mode: 'r:UTF-8', newline: :universal }
+        let(:expected) { (IO.read output_filename, mode: 'r:UTF-8', newline: :universal).chomp }
+        it 'converts inline Asciidoctor syntax to HTML' do
+          doc.to_html.must_equal expected
+        end
+      end
+    end
+  end
+end
