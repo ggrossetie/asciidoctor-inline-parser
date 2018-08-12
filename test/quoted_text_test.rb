391f1e19eb20d3dbc9fe14ade533802ff1ae24c8
Guillaume Grossetie <ggrossetie@gmail.com>
2018-08-12 19:52:31 +0200

    Remove constrained quote limitations

--- test/quoted_text_test.rb
+++ test/quoted_text_test.rb
@@ -122,53 +122,56 @@ describe 'quoted text' do
         nodes.first.text_value.must_equal input
         nodes.first.content.must_equal %(a few\nwords)
       end
-      # Constrained quote limitations
-      it 'should parse a constrained string with a letter directly outside the formatting marks' do
-        input = "There's a l#{symbol}e#{symbol}tter directly outside the formatting marks."
-        ast = ::Asciidoctor::InlineParser.parse input
-        ast.text_value.must_equal input
-        strong_nodes = find_by (node_type_must_be type), ast
-        strong_nodes.size.must_equal 0
-      end
-      # there's a space directly inside of the formatting mark?
-      it 'should parse a constrained string with inner spaces' do
-        input = "There's a #{symbol} space #{symbol} directly inside of the formatting mark"
-        ast = ::Asciidoctor::InlineParser.parse input
-        ast.text_value.must_equal input
-        strong_nodes = find_by (node_type_must_be type), ast
-        strong_nodes.size.must_equal 0
-      end
-      # there's a number directly outside the formatting marks
       it 'should parse a constrained string with a number directly outside the formatting marks' do
         input = "E = #{symbol}mc#{symbol}2"
         ast = ::Asciidoctor::InlineParser.parse input
         ast.text_value.must_equal input
-        strong_nodes = find_by (node_type_must_be type), ast
-        strong_nodes.size.must_equal 0
+        nodes = find_by (node_type_must_be type), ast
+        nodes.size.must_equal 1
+        nodes.first.text_value.must_equal "#{symbol}mc#{symbol}"
+        nodes.first.content.must_equal 'mc'
       end
-      # there's a colon directly before the starting formatting mark
       it 'should parse a constrained string with a colon directly before the starting formatting mark' do
         input = "There's a colon:#{symbol}directly#{symbol} before the starting formatting mark."
         ast = ::Asciidoctor::InlineParser.parse input
         ast.text_value.must_equal input
-        strong_nodes = find_by (node_type_must_be type), ast
-        strong_nodes.size.must_equal 0
+        nodes = find_by (node_type_must_be type), ast
+        nodes.size.must_equal 1
+        nodes.first.text_value.must_equal "#{symbol}directly#{symbol}"
+        nodes.first.content.must_equal 'directly'
       end
-      # there's a semi-colon directly before the starting formatting mark
       it 'should parse a constrained string with a semi-colon directly before the starting formatting mark' do
         input = "There's a semi-colon directly before the starting formatting mark &ndash;#{symbol}2018#{symbol}"
         ast = ::Asciidoctor::InlineParser.parse input
         ast.text_value.must_equal input
-        strong_nodes = find_by (node_type_must_be type), ast
-        strong_nodes.size.must_equal 0
+        nodes = find_by (node_type_must_be type), ast
+        nodes.first.text_value.must_equal "#{symbol}2018#{symbol}"
+        nodes.first.content.must_equal '2018'
       end
-      # there's a closing curly bracket directly before the starting formatting mark
       it 'should parse a constrained string with a closing curly bracket directly before the starting formatting mark' do
         input = "There's a closing curly bracket directly {before}#{symbol}the starting formatting mark#{symbol}."
         ast = ::Asciidoctor::InlineParser.parse input
         ast.text_value.must_equal input
-        strong_nodes = find_by (node_type_must_be type), ast
-        strong_nodes.size.must_equal 0
+        nodes = find_by (node_type_must_be type), ast
+        nodes.first.text_value.must_equal "#{symbol}the starting formatting mark#{symbol}"
+        nodes.first.content.must_equal 'the starting formatting mark'
+      end
+      ## Constrained quote limitations
+      # there's a letter directly outside the formatting marks
+      it 'should parse a constrained string with a letter directly outside the formatting marks' do
+        input = "There's a l#{symbol}e#{symbol}tter directly outside the formatting marks."
+        ast = ::Asciidoctor::InlineParser.parse input
+        ast.text_value.must_equal input
+        nodes = find_by (node_type_must_be type), ast
+        nodes.size.must_equal 0
+      end
+      # there's a space directly inside of the formatting mark
+      it 'should parse a constrained string with inner spaces' do
+        input = "There's a #{symbol} space #{symbol} directly inside of the formatting mark"
+        ast = ::Asciidoctor::InlineParser.parse input
+        ast.text_value.must_equal input
+        nodes = find_by (node_type_must_be type), ast
+        nodes.size.must_equal 0
       end
     end
   end
