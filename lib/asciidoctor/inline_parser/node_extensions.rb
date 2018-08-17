6dc1ee0af25d3233839d4da28d841e0757764e89
Guillaume Grossetie <ggrossetie@gmail.com>
2018-08-17 17:42:12 +0200

    Implement link attributes (roles, window, text)

--- lib/asciidoctor/inline_parser/node_extensions.rb
+++ lib/asciidoctor/inline_parser/node_extensions.rb
@@ -161,8 +161,10 @@ module AsciidoctorGrammar
 
   # Link
   class Link < ::Treetop::Runtime::SyntaxNode
+    BLANK_SHORTHAND = '^'.freeze
+
     def to_html
-      %(<a href="#{target}" class="#{roles.join(',')}">#{text}</a>)
+      %(<a href="#{target}" class="#{roles.join(',')}"#{window ? %( target="#{window}") : ''}>#{text}</a>)
     end
 
     def target
@@ -181,8 +183,18 @@ module AsciidoctorGrammar
 
     def text
       attrs_node = @comprehensive_elements.select { |el| el.instance_of? ::AsciidoctorGrammar::LinkAttributes }.first
-      return attrs_node.text if attrs_node
+      if attrs_node
+        text = attrs_node.text
+        return text.chop if text.end_with? BLANK_SHORTHAND
+        return text
+      end
       target
+    end
+
+    def window
+      attrs_node = @comprehensive_elements.select { |el| el.instance_of? ::AsciidoctorGrammar::LinkAttributes }.first
+      return '_blank' if attrs_node && (attrs_node.text.end_with? BLANK_SHORTHAND)
+      attrs_node.window if attrs_node && attrs_node.window
     end
 
     def roles
@@ -214,7 +226,16 @@ module AsciidoctorGrammar
 
     def text
       text_node = @comprehensive_elements.select { |el| el.instance_of? ::AsciidoctorGrammar::LinkText }.first
-      text_node.name if text_node
+      return text_node.name if text_node
+      protected_text_node = @comprehensive_elements.select do |el|
+        el.instance_of? ::AsciidoctorGrammar::LinkTextProtected
+      end.first
+      protected_text_node.name if protected_text_node
+    end
+
+    def window
+      window_node = @comprehensive_elements.select { |el| el.instance_of? ::AsciidoctorGrammar::LinkWindow }.first
+      window_node.name if window_node
     end
   end
 
@@ -230,6 +251,11 @@ module AsciidoctorGrammar
       attrs_node.roles if attrs_node
     end
 
+    def window
+      attrs_node = @comprehensive_elements.select { |el| link_attributes_content? el }.first
+      attrs_node.window if attrs_node
+    end
+
     private
 
     def link_attributes_content? node
@@ -240,7 +266,14 @@ module AsciidoctorGrammar
   # Link role
   class LinkRole < ::Treetop::Runtime::SyntaxNode
     def name
-      text_value
+      @elements[1].text_value
+    end
+  end
+
+  # Link window
+  class LinkWindow < ::Treetop::Runtime::SyntaxNode
+    def name
+      @elements[1].text_value
     end
   end
 
@@ -248,6 +281,13 @@ module AsciidoctorGrammar
   class LinkText < ::Treetop::Runtime::SyntaxNode
     def name
       text_value
+    end
+  end
+
+  # Link text protected with double quotes
+  class LinkTextProtected < ::Treetop::Runtime::SyntaxNode
+    def name
+      @elements[1].text_value
     end
   end
 
